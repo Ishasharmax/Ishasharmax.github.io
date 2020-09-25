8df8566308ce642d44b0d5d7d869e24e9053795e
Isha Sharma <ishasharma@Im-Ishas.local>
2020-09-25 16:20:51 +0530

    buttons

--- js/js1.js
+++ js/js1.js
@@ -10,3 +10,13 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+
+window.addEventListener('scroll', function() {
+  console.log("Scrollin'");
+});
+var scroll = window.requestAnimationFrame ||
+            function(callback){ window.setTimeout(callback, 1000/60)};
+
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
